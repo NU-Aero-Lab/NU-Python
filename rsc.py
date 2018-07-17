1fcec839fa2ec25112bbc6dd207a934c2de8f6b9
ravindu.t.b.ranaweera <w15041383@northumbria.ac.uk>
2018-07-17 14:43:34 +0100

    updates to python code
    added a new module for data transfer and moved the code out of the rsc module

--- rsc.py
+++ rsc.py
@@ -4,28 +4,6 @@ import time, sys, atexit, socket, struct
 from upm import pyupm_rsc as rsc
 from functools import reduce
 
-# For transfering data to server
-class DataTransfer:
-      
-    TCP_IP = '192.168.3.1'
-    TCP_PORT = 5005
-      
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.TCP_IP,self.TCP_PORT))
-        
-    def __enter__(self):
-        return self
-        
-    def sendData(self,tag, value):
-        self.sock.send(tag.encode())
-        packer = struct.Struct('f')
-        packed_data = packer.pack(value)
-        self.sock.send(packed_data)
-        
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.sock.close()
-
 # For storing and averagin pressure data
 class RscDataHandler:
     
