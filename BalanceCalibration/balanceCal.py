fb378b5b2388980f96c07016cb47bbeb9e7d1ce2
Hesham Ebrahim <Hesham.Ebrahim@hotmail.com>
2018-10-23 13:47:18 +0100

    2nd order cali.

--- BalanceCalibration/balanceCal.py
+++ BalanceCalibration/balanceCal.py
@@ -9,12 +9,21 @@ H = cal_data[:,:6]
 R = cal_data[:,6:]
 
 # Create the elongated R matrix
-#R = np.append(R,R*R,axis=1)
-#Rp = np.append(Rp,np.repeat(R[:,0][:,np.newaxis],5,axis=1)*R[:,1:],axis=1)
-#Rp = np.append(Rp,np.repeat(R[:,1][:,np.newaxis],4,axis=1)*R[:,2:],axis=1)
-#Rp = np.append(Rp,np.repeat(R[:,2][:,np.newaxis],3,axis=1)*R[:,3:],axis=1)
-#Rp = np.append(Rp,np.repeat(R[:,3][:,np.newaxis],2,axis=1)*R[:,4:],axis=1)
-#Rp = np.append(Rp,(R[:,4]*R[:,5])[:,np.newaxis],axis=1)
+Rp = np.append(R,R*R,axis=1)
+print('Rp1=', Rp)
+Rp = np.append(Rp,np.repeat(R[:,0][:,np.newaxis],5,axis=1)*R[:,1:],axis=1)
+print('Rp2=', Rp)
+Rp = np.append(Rp,np.repeat(R[:,1][:,np.newaxis],4,axis=1)*R[:,2:],axis=1)
+print('Rp3=', Rp)
+Rp = np.append(Rp,np.repeat(R[:,2][:,np.newaxis],3,axis=1)*R[:,3:],axis=1)
+print('Rp4=', Rp)
+Rp = np.append(Rp,np.repeat(R[:,3][:,np.newaxis],2,axis=1)*R[:,4:],axis=1)
+print('Rp5=', Rp)
+Rp = np.append(Rp,(R[:,4]*R[:,5])[:,np.newaxis],axis=1)
+
+print('Rp6=', Rp)
+
+print('checking: ', np.repeat(R[:,0][:,np.newaxis],5))
 
 # Compute the coefficient matrix
 R2 = np.matmul(R.transpose(),R)
@@ -25,10 +34,11 @@ C = np.matmul(Rinv,H)
 # Get the modelled loads
 Hp = np.matmul(R,C)
 
-saveData = np.savetxt("C.csv", C, delimiter=",")
+#saveData = np.savetxt("C.csv", C, delimiter=",")
 
+# Compute error
 err = np.sqrt((np.sum((Hp-H)**2,axis=0))/(H.shape[0]-6))
-err_norm = (err / np.array([120,60,240,25,20,135]))*100
+err_norm = (err / np.array([120,60,240,28,44,24]))*100
 
 # Plot the results
 spv = [321, 323, 325, 322, 324, 326]
