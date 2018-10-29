bdd7b827bcbbdca24c0699d27c775dfe8a6da129
Hesham Ebrahim <Hesham.Ebrahim@hotmail.com>
2018-10-29 20:54:41 +0000

    Bounds Included

--- DragPrediction/dragPrediction.py
+++ DragPrediction/dragPrediction.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
+from scipy.optimize import Bounds
+from scipy.optimize import LinearConstraint
 
 # Load the data in
 cp_data = np.genfromtxt('2CarPlatoon-Leader_0.5L.csv', delimiter=',',skip_header=True)
@@ -16,19 +18,34 @@ CpFront = cp_data[front,3]
 CpRear = cp_data[~front,3]
 
 # Populate initial coefficients
-#coefs0 = np.ones((np.size(CpFront)+np.size(CpRear),1)).flatten()
-coefs0 = np.ones((np.size(CpFront),1)).flatten()
+coefs0 = np.ones((np.size(CpFront)+np.size(CpRear),1)).flatten()
 
 # Functions 
-def ForceF(Coefs, CpF, Area): return np.sum(CpF*Coefs*Area)
-#def ForceR(Coefs, CpF, CpR, Area): return sum(CpF.iterable*Coefs*Area)
+def ForceF(Coefs, CpF, Area): 
+    CoefsF = Coefs[0:np.size(CpF)].flatten() 
+    return np.sum(CpF*CoefsF*Area)
 
-def Fitness(Coefs, CpF, Area, knownF):
-    return (knownF-ForceF(Coefs, CpF, Area))**2
+def ForceR(Coefs, CpF, CpR, Area): 
+    CoefsR = Coefs[np.size(CpF):np.size(Coefs)].flatten()
+    return np.sum(CpR*CoefsR*Area)
 
-# Fitness function
-#FitnessFunction = (@coefs ((CD_F-ForceF(coefs0,CpFront,Area))**2))#+(CD_R-ForceR(coefs,CpFront,CpRear,Area))^2)
+def Fitness(Coefs, CpF, CpR, Area, knownF, knownR):
+    return ((knownF-ForceF(Coefs, CpF, Area))**2)+((knownR-ForceR(Coefs, CpF, CpR, Area))**2)
 
-res = minimize(Fitness, coefs0, method='nelder-mead',options={'xtol' : 1e-8, 'disp' : True}, args=(CpFront,Area,CD_F))
+# Solution bounds
+ub = np.full((1,np.size(coefs0)), 1.2).flatten() 
+lb = np.full((1,np.size(coefs0)), -1.2).flatten()
+bounds = Bounds(lb,ub)
 
-print(res)
+res = minimize(Fitness, coefs0, method='TNC',options={'xtol' : 1e-8, 'disp' : True}, args=(CpFront,CpRear,Area,CD_F,CD_R), bounds=bounds)
+print('ForceF= ', ForceF(res.x, CpFront, Area))
+print('ForceR= ', ForceR(res.x, CpFront, CpRear, Area))
+
+plt.plot(res.x, 'o', markersize=4, label='coefs')
+plt.xlabel("Coefficient No.")
+plt.ylabel("Scaling")
+# plt.legend(loc='lower right')
+plt.show()
+
+# saveData = np.savetxt("test.csv", res.x, delimiter=",")
+
