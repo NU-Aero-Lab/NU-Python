01fac556330545baf5733932d375f21eb4388d03
Hesham Ebrahim <Hesham.Ebrahim@hotmail.com>
2018-11-19 18:20:20 +0000

    multiRunDragPrediction
    Combines all the platoon configurations' data and optimises the coefs'.

--- DragPrediction/multiRunDragPrediction.py
+++ DragPrediction/multiRunDragPrediction.py
@@ -1,46 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import Functions as funct
 from scipy.optimize import minimize
 from scipy.optimize import Bounds
 from scipy.optimize import differential_evolution
 
-# add all the raw data into an array and create a for loop that goes through all 
-# the information at once and displays all the figures 
-
-spv = [321, 323, 325, 322, 324, 326]
-files = ['NL2VP_0.5L.csv','NL2VP_0.75L.csv']
-for i, ind in enumerate(spv):
-
-# Load the data in
-cp_data = np.genfromtxt('NL2VP_0.75T.csv', delimiter=',',skip_header=True)
+# Load data
+files = ['NL2VP_0.5L.csv','NL2VP_0.5T.csv','NL2VP_0.75L.csv','NL2VP_0.75T.csv','NL2VP_1L.csv','NL2VP_1T.csv']
 
 # Variables
-Area = 0.0885*0.11167 
-CD_F = 0.0572 # This is the drag coefficient of the whole front half of the car
-CD_R = 0.0871
+Area = 0.0885*0.11167
+CD_F = [0.0315, 0.0721, 0.0295, 0.0572, 0.0292, 0.0466]
+CD_R = [0.0396, 0.0895, 0.0622, 0.0871, 0.074, 0.087]
+caseArray = []
 
-# Extract and Split Cp 
-front = cp_data[:,0] < -2 
-CpFront = cp_data[front,3]
-CpRear = cp_data[~front,3]
-normalFront = cp_data[front,4]
-normalRear = cp_data[~front,4]
+# Set cases in an Array
+for i, ind in enumerate(files):
 
-# Populate initial coefficients
-coefs0 = np.ones((np.size(CpFront)+np.size(CpRear),1)).flatten()
+    # Load the data in
+    cp_data = np.genfromtxt(ind, delimiter=',',skip_header=True)     
 
-# Functions 
-def ForceF(Coefs, CpF, Area, NF): 
-    CoefsF = Coefs[0:np.size(CpF)].flatten() 
-    
-    return np.sum(CpF*CoefsF*Area*NF)
+    # Extract and Split Cp 
+    front = cp_data[:,0] < -2 
+    CpFront = cp_data[front,3]
+    CpRear = cp_data[~front,3]
+    normalFront = cp_data[front,4]
+    normalRear = cp_data[~front,4]
+    # Populate initial coefficients
+    coefs0 = np.ones((np.size(CpFront)+np.size(CpRear),1)).flatten()
 
-def ForceR(Coefs, CpF, CpR, Area, NR): 
-    CoefsR = Coefs[np.size(CpF):np.size(Coefs)].flatten()
-    return np.sum(CpR*CoefsR*Area*NR)
+    # Add data into caseArray
+    Case = [[CpFront], [CpRear], [normalFront], [normalRear], [coefs0]]
+    caseArray.append(Case)  
 
-def Fitness(Coefs, CpF, CpR, Area, NF, NR, knownF, knownR):
-    return ((knownF-ForceF(Coefs, CpF, Area, NF))**2)+((knownR-ForceR(Coefs, CpF, CpR, Area, NR))**2)
+coefs0 = np.hstack(caseArray[:5])
+coefs0 = np.hstack(coefs0[4])
+
+# Fitness Function
+def Fitness(Coefs, Area, knownF, knownR):
+    return (((knownF[0]-funct.ForceF1(Coefs, caseArray, Area))**2)+((knownR[0]-funct.ForceR1(Coefs, caseArray, Area))**2)+
+    ((knownF[2]-funct.ForceF2(Coefs, caseArray, Area))**2)+((knownR[2]-funct.ForceR2(Coefs, caseArray, Area))**2)+
+    ((knownF[4]-funct.ForceF3(Coefs, caseArray, Area))**2)+((knownR[4]-funct.ForceR3(Coefs, caseArray, Area))**2))
+
+# print(Fitness(Coefs, Area, knownF, knownR))
+
 
 # Solution bounds
 ub = np.full((1,np.size(coefs0)), 1.3).flatten() 
@@ -50,38 +53,38 @@ bounds = Bounds(lb,ub)
 boundaries = ((1.2,-1.2),)*(len(coefs0)) # boundaries for genetic algorithm
 
 # minimise function
-res = minimize(Fitness, coefs0, method='TNC', options={'accuracy' : 1e-20, 'xtol' : 1e-20,'ftol' : 1e-20, 'disp' : True}, args=(CpFront,CpRear,Area,normalFront,normalRear,CD_F,CD_R), bounds=bounds)
+res = minimize(Fitness, coefs0, method='TNC', options={'accuracy' : 1e-20, 'xtol' : 1e-20,'ftol' : 1e-20, 'disp' : True}, args=(Area,CD_F,CD_R), bounds=bounds)
 
 # genetic algorithm (differential evolution)
 # res = differential_evolution(Fitness, bounds=boundaries, args=(CpFront,CpRear,Area,normalFront,normalRear,CD_F,CD_R), strategy='best1bin', maxiter=10, popsize=15, tol=0.5, mutation=(0.5,1),
 #                             recombination= 0.7, disp=True)
 
 # print result
-print('ForceF= ', ForceF(res.x, CpFront, Area, normalFront), '  %Diff=', (CD_F-ForceF(res.x, CpFront, Area, normalFront))/CD_F*100)
-print('ForceR= ', ForceR(res.x, CpFront, CpRear, Area, normalRear), '  %Diff=', (CD_R-ForceR(res.x, CpFront, CpRear, Area, normalRear))/CD_R*100)
+# print('ForceF= ', ForceF1(res.x, CpFront, Area, normalFront), '  %Diff=', (CD_F[0]-ForceF1(res.x, CpFront, Area, normalFront))/CD_F[0]*100)
+# print('ForceR= ', ForceR(res.x, CpFront, CpRear, Area, normalRear), '  %Diff=', (CD_R-ForceR(res.x, CpFront, CpRear, Area, normalRear))/CD_R*100)
 
-# plots
-x1 = np.linspace(0, np.size(CpFront), num=np.size(CpFront))
-plt.subplot(221)
-plt.plot(x1, CpFront*normalFront*coefs0[0:np.size(CpFront)].flatten() , '-', label='Ref', linewidth=2, color='r')
-plt.plot(x1, CpFront*normalFront*res.x[0:np.size(CpFront)].flatten() , '-')
-plt.xlabel("Point Number")
-plt.ylabel("Cp")
-plt.legend()
+# # plots
+# x1 = np.linspace(0, np.size(CpFront), num=np.size(CpFront))
+# plt.subplot(221)
+# plt.plot(x1, CpFront*normalFront*coefs0[0:np.size(CpFront)].flatten() , '-', label='Ref', linewidth=2, color='r')
+# plt.plot(x1, CpFront*normalFront*res.x[0:np.size(CpFront)].flatten() , '-')
+# plt.xlabel("Point Number")
+# plt.ylabel("Cp")
+# plt.legend()
 
-x2 = np.linspace(0, np.size(CpRear), num=np.size(CpRear))
-plt.subplot(222)
-plt.plot(x2, CpRear*normalRear*coefs0[0:np.size(CpRear)].flatten(), '-', label='Ref', linewidth=2, color='r')
-plt.plot(x2, CpRear*normalRear*res.x[np.size(CpFront):np.size(coefs0)].flatten() , '-')
-plt.xlabel("Point Number")
-plt.ylabel("Cp")
-plt.legend()
+# x2 = np.linspace(0, np.size(CpRear), num=np.size(CpRear))
+# plt.subplot(222)
+# plt.plot(x2, CpRear*normalRear*coefs0[0:np.size(CpRear)].flatten(), '-', label='Ref', linewidth=2, color='r')
+# plt.plot(x2, CpRear*normalRear*res.x[np.size(CpFront):np.size(coefs0)].flatten() , '-')
+# plt.xlabel("Point Number")
+# plt.ylabel("Cp")
+# plt.legend()
 
-plt.subplot(212)
-plt.plot(res.x, 'o', markersize=4, label='coefs')
-plt.xlabel("Coefficient No.")
-plt.ylabel("Scaling")
-plt.show()
+# plt.subplot(212)
+# plt.plot(res.x, 'o', markersize=4, label='coefs')
+# plt.xlabel("Coefficient No.")
+# plt.ylabel("Scaling")
+# plt.show()
 
 # saveData = np.savetxt("test.csv", res.x, delimiter=",")
 
